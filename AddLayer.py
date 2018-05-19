3bcbe9bb67170491493eea35aab1a7e51faf78a3
oyamax <ilove081>
2018-05-20 02:04:48 +0900

    NeuralNetworkお勉強

---
+++ AddLayer.py
@@ -0,0 +1,12 @@
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout
+        dy = dout
+        return dx, dy
